adae1fc5e3e283e55c9ddd76728416c68cfd5891
Plinio Mabesi <plinoimabesi@gmail.com>
2020-07-16 22:07:53 -0300

    Keyboard layout and callback function adjusted

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
 import '../components/display.dart';
+import '../components/keyboard.dart';
 
 class Calculator extends StatelessWidget {
+
+  _onPressed(String text) {
+    print(text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Column(
         children: <Widget>[
           Display('123.45'),
-          Text('Keyboard'),
+          Keyboard(_onPressed),
         ],
       ),
     );
