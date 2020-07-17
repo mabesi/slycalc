7f04e3ce347bd045f373ee9f7a18992d1c151d7a
Plinio Mabesi <plinoimabesi@gmail.com>
2020-07-16 22:31:58 -0300

    Device orientation preference adjusted

--- lib/screens/calculator.dart
+++ lib/screens/calculator.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../components/display.dart';
 import '../components/keyboard.dart';
 import '../models/memory.dart';
@@ -22,6 +23,9 @@ class _CalculatorState extends State<Calculator> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Column(
